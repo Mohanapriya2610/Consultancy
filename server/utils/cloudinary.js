9e7b53bcf5251af3e300bf07d4dc6a5d24910ac4
nandhas20ITR069 <88595743+nandhas20ITR069@users.noreply.github.com>
2023-07-04 19:25:37 +0530

    new

--- server/utils/cloudinary.js
+++
@@ -1,9 +0,0 @@
-const cloudinary = require('cloudinary').v2;
-
-
-  cloudinary.config({ 
-    cloud_name: 'ddfjo8m7z', 
-    api_key: '724129873798785', 
-    api_secret: 'szCCd4PyhMP4rfaQLKZiIWew62M' 
-  });
-  module.exports = cloudinary
